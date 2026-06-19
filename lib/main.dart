bb28c3aec6602ee9d98a08740fcc8bdbe3a9ca6f
V S Sujithraa <sujithraasudhakar02@gmail.com>
2026-06-19 20:30:48 +0530

    feat(backend): Implement Backend Services for All App Features

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,32 @@
+import 'screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-import 'app.dart';
-import 'firebase_init.dart';
-
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initFirebase();
-  runApp(const App());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const ExcelerateApp());
 }
 
+class ExcelerateApp extends StatelessWidget {
+  const ExcelerateApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Excelerate Pathfinder',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE0194A),
+        scaffoldBackgroundColor: const Color(0xFFF7F7F7),
+        fontFamily: 'Roboto',
+      ),
+       home: const SplashScreen(),
+    );
+  }
+}
